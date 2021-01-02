554fe927b944fc86cd4dad464d98537fcf9b1331
jeffreygatbonton <jeff@creationcrate.com>
2021-01-02 12:55:29 -0500

    set up basic crud with mikro orm and typegraphql

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { MyContext } from 'src/types';
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/Post';
 
 @Resolver()
@@ -9,11 +9,55 @@ export class PostResolver {
     return em.find(Post, {});
   }
 
+  // read
   @Query(() => Post, { nullable: true })
   post(
-    @Arg('id', () => Int) id: number,
+    // arg is what the query will be called (id), typescript type follows
+    @Arg('id') id: number,
+    // context is my mikro orm shit
+    @Ctx() { em }: MyContext
+    // this query returns a promise here
+  ): Promise<Post | null> {
+    // do stuff here
+    return em.findOne(Post, { id });
+  }
+
+  // create
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title })
+    await em.persistAndFlush(post)
+    return post;
+  }
+
+  // update
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
-    return em.findOne(Post, { id });
+    const post = await em.findOne(Post, { id })
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  // delete
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id })
+    return true;
   }
 }
