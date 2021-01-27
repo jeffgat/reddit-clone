8453dae071ae8cd0f84ebaf8dbf4322406db7936
jeffgat <jeff.gtbn@gmail.com>
2021-01-26 16:40:33 -0800

    added some back buttons

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,17 +1,18 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { Box, Button, Heading, Link, Text } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/router';
 import React from 'react';
 import EditDeletePostButtons from '../../components/EditDeletePostButtons';
 import Layout from '../../components/Layout';
-import { usePostQuery, usePostsQuery } from '../../generated/graphql';
 import { createUrqlClient } from '../../utils/createUrqlClient';
 import { useGetPostFromUrl } from '../../utils/useGetPostFromUrl';
 
 interface Props {}
 
 const Post = (props: Props) => {
-  const [{ data, error, fetching }] = useGetPostFromUrl();
+  const [{ data, fetching }] = useGetPostFromUrl();
+  const router = useRouter();
 
   if (fetching) {
     <Box>loading...</Box>;
@@ -28,10 +29,24 @@ const Post = (props: Props) => {
   return (
     <Layout>
       <Box mb={2}>
+        <Button
+          as={Link}
+          fontWeight='medium'
+          variant='solid'
+          ml='auto'
+          mb='6'
+          leftIcon={<ArrowBackIcon w={3} h={3} />}
+          onClick={() => router.back()}
+        >
+          Back
+        </Button>
         <Heading mb={4}>{data.post.title}</Heading>
         <Text>{data.post.text}</Text>
       </Box>
-      <EditDeletePostButtons id={data.post.id} creatorId={data.post.creator.id}/>
+      <EditDeletePostButtons
+        id={data.post.id}
+        creatorId={data.post.creator.id}
+      />
     </Layout>
   );
 };
